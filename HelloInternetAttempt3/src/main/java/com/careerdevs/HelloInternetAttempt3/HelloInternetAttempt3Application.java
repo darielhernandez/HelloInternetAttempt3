2ac8e392ea73411060b960f318728dbadef77764
darielhernandez <13dariel@gmail.com>
2022-03-07 17:10:07 -0500

    HelloInternetAttempt3Application.java Internet.
    "You created the root route working"

--- HelloInternetAttempt3/src/main/java/com/careerdevs/HelloInternetAttempt3/HelloInternetAttempt3Application.java
+++ HelloInternetAttempt3/src/main/java/com/careerdevs/HelloInternetAttempt3/HelloInternetAttempt3Application.java
@@ -2,9 +2,17 @@ package com.careerdevs.HelloInternetAttempt3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloInternetAttempt3Application {
+
+	@GetMapping ("/")
+	public String rootRoute(){
+		return "You requested the root route";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloInternetAttempt3Application.class, args);
